a4927d0573b5f24e5a00844d83b741e2c4116f1d
kennethlng <ng.kennethlee@gmail.com>
2015-07-10 09:57:34 -0700

    Added 3 button types

---
+++ MotherNatureGit/Assets/Scripts/ButtonFlick.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonFlick : MonoBehaviour {
+
+	public bool isClicked;
+
+	void Start ()
+	{
+		isClicked = false;
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Player") 
+		{
+			isClicked = !isClicked;
+			ChangeColor();
+		}
+	}
+
+	void ChangeColor ()
+	{
+		switch (isClicked) 
+		{
+		case true:
+			GetComponentInChildren<MeshRenderer>().material.SetColor("_Color", Color.blue);
+			break;
+		case false:
+			GetComponentInChildren<MeshRenderer>().material.SetColor("_Color", Color.red);
+			break;
+		}
+	}
+
+}
